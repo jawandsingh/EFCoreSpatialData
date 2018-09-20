1561ec27b8a7a7f77ab3572a34892a8aeb3931d1
Jawand Singh <jawand.virk@gmail.com>
2018-09-20 23:55:12 +0530

    added an entity named GeoLocation and appsettings.json

---
+++ EFCore.SpatialData/Entities/GeoLocation.cs
@@ -0,0 +1,15 @@
+﻿using NetTopologySuite.Geometries;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace EFCore.SpatialData.Entities
+{
+	public class GeoLocation
+	{
+		[Key]
+		[DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+		public int Id { get; set; }
+		public Point Location { get; set; }
+		public string Address { get; set; }
+	}
+}
