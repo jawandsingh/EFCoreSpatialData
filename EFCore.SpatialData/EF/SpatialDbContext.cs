dad6a8d577bf5c3949d50c235becc71d887e2ae4
Jawand Singh <jawand.virk@gmail.com>
2018-09-20 23:54:37 +0530

    added DbContext class.

---
+++ EFCore.SpatialData/EF/SpatialDbContext.cs
@@ -0,0 +1,28 @@
+﻿using EFCore.SpatialData.Entities;
+using Microsoft.EntityFrameworkCore;
+using NetTopologySuite.Geometries;
+
+namespace EFCore.SpatialData.EF
+{
+	public class SpatialDbContext : DbContext
+	{
+		public SpatialDbContext(DbContextOptions<SpatialDbContext> options) : base(options)
+		{
+
+		}
+
+		public DbSet<GeoLocation> GeoLocations { get; set; }
+		
+		public void AddRemoveGeoLocation()
+		{
+			Database.EnsureDeleted();
+			Database.EnsureCreated();
+
+			AddRange(new GeoLocation { Address = "Agra", Location = new Point(27.1767, 78.0081) },
+					 new GeoLocation { Address = "Varanasi", Location = new Point(25.3176, 82.9739) },
+					 new GeoLocation { Address = "New Delhi", Location = new Point(28.7041, 77.1025) },
+					 new GeoLocation { Address = "Jaipur", Location = new Point(26.9124, 75.7873) });
+			SaveChanges();
+		}
+	}
+}
---
+++ EFCore.SpatialData/EF/SpatialDbContextFactory.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EFCore.SpatialData.EF
+{
+	public class SpatialDbContextFactory : IDesignTimeDbContextFactory<SpatialDbContext>
+	{
+		private static string _connectionString;
+
+		public SpatialDbContext CreateDbContext()
+		{
+			return CreateDbContext(null);
+		}
+
+		public SpatialDbContext CreateDbContext(string[] args)
+		{
+			if (string.IsNullOrEmpty(_connectionString))
+			{
+				LoadConnectionString();
+			}
+
+			var builder = new DbContextOptionsBuilder<SpatialDbContext>();
+			builder.UseSqlServer(_connectionString, 
+								sqlOption => sqlOption.UseNetTopologySuite());
+
+			return new SpatialDbContext(builder.Options);
+		}
+
+		private static void LoadConnectionString()
+		{
+			ConfigurationBuilder builder = new ConfigurationBuilder();
+			builder.AddJsonFile("appsettings.json", optional: false);
+
+			IConfigurationRoot configuration = builder.Build();
+
+			_connectionString = configuration.GetConnectionString("DefaultConnection");
+
+			if (string.IsNullOrEmpty(_connectionString))
+				throw new Exception("Not able to load connection string from appsettings.json");
+		}
+	}
+}
