21eb8db0df8be9fee8250f50142ce4c81911a071
Jawand Singh <jawand.virk@gmail.com>
2018-09-30 19:43:20 +0530

    added comments

--- EFCore.SpatialData/EF/SpatialDbContext.cs
+++ EFCore.SpatialData/EF/SpatialDbContext.cs
@@ -15,9 +15,15 @@ namespace EFCore.SpatialData.EF
 		
 		public void AddRemoveGeoLocation()
 		{
+			//Ensures that the database for the context does not exist. If it does not exist, 
+			//no action is taken. If it does exist then the database is deleted.
 			Database.EnsureDeleted();
-			Database.EnsureCreated();
 
+			//Ensures that the database for the context exists. If it exists, no action is taken.
+			//If it does not exist then the database and all its schema are created.
+			Database.EnsureCreated();
+			
+			// Adding some Spatial Data.
 			AddRange(new GeoLocation { Address = "Agra", Location = new Point(27.1767, 78.0081) },
 					 new GeoLocation { Address = "Varanasi", Location = new Point(25.3176, 82.9739) },
 					 new GeoLocation { Address = "New Delhi", Location = new Point(28.7041, 77.1025) },
